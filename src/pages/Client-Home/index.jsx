4e074fd10b092b3fa192887d222d45d91e9e4197
diogo <diogobrasildasilva@gmail.com>
2024-05-31 14:49:20 -0300

    Adição da rota para PropertyDetails.

--- src/pages/Client-Home/index.jsx
+++ src/pages/Client-Home/index.jsx
@@ -26,7 +26,7 @@ export function ClientHome() {
           >
             <SwiperSlide>
 
-              <PropertyCard to={"/profile"}
+              <PropertyCard to={"/property/:id"}
                 title={"Residencial Jardim Botânico"}
                 img={"https://plus.unsplash.com/premium_photo-1687960117069-567a456fe5f3?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
                 description={"2 quartos, piscina e vista para o mar"}
